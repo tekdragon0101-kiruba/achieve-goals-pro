b49feb7bd3266aa894b11fdd2cf42f64b2d05191
Kirubakaran K <gkalidassfarmer1963@gmail.com>
2025-04-25 13:44:36 +0000

    package.json file cds fixed

--- srv/service.js
+++ srv/service.js
@@ -1,5 +1,6 @@
 const cds = require('@sap/cds');
 const { SELECT } = require('@sap/cds/lib/ql/cds-ql');
+const { find } = require('@sap/cds/lib/utils/cds-utils');
 
 module.exports = class AchieveGoalsService extends cds.ApplicationService {
     init() {
@@ -26,13 +27,25 @@ module.exports = class AchieveGoalsService extends cds.ApplicationService {
     }
     async sequenceGenerator(req, entity) {
         if (req.subject?.ref && String(req.subject?.ref[0]).search('drafts') !== -1) {
-            const maxTaskNo = await SELECT.one.from(entity).columns('max(id) as maxId');
-            const maxTaskNoDraft = await SELECT.one.from(entity.drafts).columns('max(id) as maxId');
-            const maxTask = (maxTaskNo.maxId > maxTaskNoDraft.maxId ? maxTaskNo : maxTaskNoDraft);
+            // const maxTaskNo = await SELECT.one.from(entity).columns('max(id) as maxId');
+            // const maxTaskNoDraft = await SELECT.one.from(entity.drafts).columns('max(id) as maxId');
+            const maxTask = await this.findMaxId(entity);
+            // If no existing entries, start from 1, else increment
+            req.data.id = (maxTask?.maxId || 0) + 1;
+            return;
+        }
+        if (!req.data.id) {
+            const maxTask = await this.findMaxId(entity);
             // If no existing entries, start from 1, else increment
             req.data.id = (maxTask?.maxId || 0) + 1;
             return;
         }
         return;
+    }
+    async findMaxId(entity) {
+        const maxTaskNo = await SELECT.one.from(entity).columns('max(id) as maxId');
+        const maxTaskNoDraft = await SELECT.one.from(entity.drafts).columns('max(id) as maxId');
+        const maxTask = (maxTaskNo.maxId > maxTaskNoDraft.maxId ? maxTaskNo : maxTaskNoDraft);
+        return maxTask;
     }
 }
